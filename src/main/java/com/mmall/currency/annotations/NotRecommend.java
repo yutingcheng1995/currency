7814aeddd342727ac1ed240e8b53e46ed5e28e79
223 <472500517@qq.com>
2019-04-02 19:08:21 +0800

    add annotations

--- src/main/java/com/mmall/currency/annotations/NotRecommend.java
+++ src/main/java/com/mmall/currency/annotations/NotRecommend.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 课程里用来标记线程安全的类或者写法
+ * 课程里用来标记不好的写法
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
